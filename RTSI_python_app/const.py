76c3f781ae6434e00a3b31e86dbc5cfd3e52f919
NikolayBlip <nikolayhornplayer@yandex.ru>
2026-07-05 13:29:15 +0300

    Create const.py

---
+++ RTSI_python_app/const.py
@@ -0,0 +1,10 @@
+eliteIP = "192.168.13.178"
+
+elitePort_29999 = 29999
+elitePort_30001 = 30001
+elitePort_30004 = 30004
+elitePort_30011 = 30011
+elitePort_30020 = 30020
+elitePort_40011 = 40011
+
+DEFAULT_TIMEOUT = 10.0
